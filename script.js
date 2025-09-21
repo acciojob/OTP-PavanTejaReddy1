ff7e94217fe447da84592a844e5a4255b897057a
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-21 17:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, i) => {
 	input.addEventListener("input", ()=>{
-		if(input.value.length == 1) {
+		if(input.value.length == 1 && i < inputs.length-1) {
 			inputs[i + 1].focus();
-		} else if (input.value == "") {
-			inputs[i - 1].focus();
+		} 
+	});
+	
+	input.addEventListener("keydown", (e)=>{
+		if(e.key == "Backspace") {
+			if(input.value == "" && i > 0) {
+				inputs[i - 1].focus();
+			}
 		}
 	})
 })
