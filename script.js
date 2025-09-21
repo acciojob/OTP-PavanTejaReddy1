7191343abc796d2428c36a22436d500d84acd7e4
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-21 13:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, i) => {
+	input.addEventListener("input", ()=>{
+		if(input.value.length == 1) {
+			inputs[i + 1].focus();
+		} else if (input.value == "") {
+			inputs[i - 1].focus();
+		}
+	})
+})
