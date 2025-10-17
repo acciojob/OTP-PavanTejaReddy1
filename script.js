39e198baba73f4b7c8cae1553b2937fbf14439c6
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-10-17 16:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 const inputs = document.querySelectorAll(".code");
+window.addEventListener("DOMContentLoaded", () => {
+  inputs[0].focus();
+})
 
 inputs.forEach((input, i) => {
-	input.addEventListener("input", (e) => {
-		e.preventDefault();
-		if (e.inputType === "insertText" && input.value.length === 1 && i < inputs.length - 1) {
-			inputs[i + 1].focus();
-		}
-	});
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    if (value && i < inputs.length - 1) {
+      inputs[i + 1].focus();
+    }
+  });
 
-	input.addEventListener("keydown", (e) => {
-		if (e.key === "Backspace") {
-			e.preventDefault();
-			if (input.value) {
-				input.value="";
-			} else {
-				if(inputs[i-1]){
-					inputs[i-1].focus();
-				}
-			}
-		}
-	});
-	input.addEventListener("focus", () => {
-		input.select();
-	});
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      e.preventDefault();
+      if (input.value) {
+        input.value = "";
+      } else if (i > 0) {
+        inputs[i - 1].focus();
+      }
+    }
+  });
 
+  input.addEventListener("focus", () => input.select());
 });
